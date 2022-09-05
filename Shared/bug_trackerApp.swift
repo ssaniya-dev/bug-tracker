0b17fada92c522ffab9229089a3f5e76c79f1e73
Saniya Savla <ssaniyasavla@gmail.com>
2022-09-05 15:29:49 -0400

    Initial Commit

---
+++ Shared/bug_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  bug_trackerApp.swift
+//  Shared
+//
+//  Created by Saniya Savla on 9/5/22.
+//
+
+import SwiftUI
+
+@main
+struct bug_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
